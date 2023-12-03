de3a73cf58d82bcc8378e6bea163e1b48de2f1b0
Emil Tovborg-Jensen <emiltovborg-jensen@Emils-MacBook-Pro.local>
2023-12-03 21:20:28 +0100

    More player logic added predictions for player move

--- play.py
+++ play.py
@@ -6,6 +6,7 @@ import torch.nn
 from torchvision import models
 from PIL import Image
 from torchvision import transforms
+import time
 
 rounds: int = int(input("Enter number of rounds: "))
 # Base case
@@ -19,14 +20,14 @@ def computer_move():
     return random.choice(moves)
 
 
-def player_move_from_image(image, model_path, class_labels):
+def player_move_from_image(image, model_path):
+    # Convert numpy array to PIL Image
+    image = Image.fromarray(image)
     model = models.resnet18(pretrained=False)
     num_classes = 4
     model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
     model.load_state_dict(torch.load(model_path))
     model.eval()  # Set the model to evaluation mode
-
-    image = Image.open(image).convert('RGB')
 
     # Transforms
     transform = transforms.Compose([
@@ -86,17 +87,34 @@ def play():
         height, width, channels = frame.shape
 
         rect_width, rect_height = 300, 300
-        rect_x, rect_y = int((width - rect_width) / 2) + 300, int((height - rect_height) / 2)
+
+        # Player move rectangle and text
+        rect_x, rect_y = int((width - rect_width) / 2) + 300, int((height - rect_height) / 2)  # Adjusted rect_x
 
         cv2.rectangle(frame, (rect_x, rect_y), (rect_x + rect_width, rect_y + rect_height), (0, 255, 0), 2)
-        text_x = rect_x + 20  # Shift the text to the right by adding 20 pixels
-        cv2.putText(frame, "Player Move", (text_x-5, rect_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
-                    (36, 255, 12), 2)
-        text = f"Round {round_number} of {rounds}"
+
+        text = "Player Move"
         text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
-        text_x = 10
-        text_y = text_size[1] + 20
-        cv2.putText(frame, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
+        text_x = rect_x + int((rect_width - text_size[0]) / 2)
+        text_y = rect_y - 10  # Adjust vertical position above the rectangle
+
+        cv2.putText(frame, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # Computer move rectangle and text
+        cv2.rectangle(frame, (rect_x, rect_y), (rect_x + rect_width, rect_y + rect_height), (0, 255, 0), 2)
+        rect_x, rect_y = int((width - rect_width) / 2) - 300, int((height - rect_height) / 2)  # Adjusted rect_x
+
+        cv2.rectangle(frame, (rect_x, rect_y), (rect_x + rect_width, rect_y + rect_height), (0, 255, 0), 2)
+
+        text = "Computer Move"
+        text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+        text_x = rect_x + int((rect_width - text_size[0]) / 2)
+        text_y = rect_y - 10  # Adjust vertical position above the rectangle
+
+        cv2.putText(frame, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # Round text
+
 
         if round_number < rounds:
             text = f"Press 'c' to lock in your move"
@@ -109,9 +127,45 @@ def play():
         text_y = text_size[1] + 10
         cv2.putText(frame, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
 
+
+
+        text = f"Round {round_number} of {rounds}"
+        text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+        text_x = 10
+        text_y = text_size[1] + 20
+        cv2.putText(frame, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
+
+        play_key = cv2.waitKey(1)
+        if play_key == ord('c'):
+            # Player logic
+            if round_number < rounds:
+                round_number = round_number + 1 if round_number > 0 else round_number
+                print(f"Starting round {round_number}")
+
+                # Capture player move image
+                rect_x, rect_y = int((width - rect_width) / 2) + 300, int((height - rect_height) / 2)  # Adjusted rect_x
+
+                time.sleep(0.5)
+                ret, frame = cap.read()
+                roi = frame[rect_y:rect_y + rect_height, rect_x:rect_x + rect_width]
+                player_move = player_move_from_image(roi, "model.pth")
+                print(f"Player move: {player_move}")
+
+            else:
+                # Determine winner
+                print("Done playing, determining winner...")
+                if player_score > computer_score:
+                    print("Player wins!")
+                elif computer_score > player_score:
+                    print("Computer wins!")
+                else:
+                    print("It's a tie!")
+
         cv2.imshow("Rock Paper Scissors", frame)
+
         key = cv2.waitKey(1)
         if key == 27:
+            print("Thanks for playing!")
             break
 
 
