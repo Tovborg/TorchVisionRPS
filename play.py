ea6d2126bdb787082f4052a91ef19854dd3bdcf8
Emil Tovborg-Jensen <emiltovborg-jensen@Emils-MacBook-Pro.local>
2023-12-03 19:30:41 +0100

    More logic added to play.py

--- play.py
+++ play.py
@@ -53,3 +53,67 @@ def player_move_from_image(image, model_path, class_labels):
     return predicted_label
 
 
+def determine_winner(move_computer, move_player):
+
+    combinations = {
+        "rock": "scissors",
+        "paper": "rock",
+        "scissors": "paper"
+    }
+
+    if move_player == combinations[move_computer]:
+        return "computer"
+    elif move_computer == combinations[move_computer]:
+        return "player"
+    else:
+        return "tie"
+
+
+def play():
+    player_score = 0
+    computer_score = 0
+    round_number = 1
+
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Cannot connect to video source, exiting...")
+        exit()
+
+    while True:
+        ret, frame = cap.read()
+
+        height, width, channels = frame.shape
+
+        rect_width, rect_height = 300, 300
+        rect_x, rect_y = int((width - rect_width) / 2) + 300, int((height - rect_height) / 2)
+
+        cv2.rectangle(frame, (rect_x, rect_y), (rect_x + rect_width, rect_y + rect_height), (0, 255, 0), 2)
+        text_x = rect_x + 20  # Shift the text to the right by adding 20 pixels
+        cv2.putText(frame, "Player Move", (text_x-5, rect_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
+                    (36, 255, 12), 2)
+        text = f"Round {round_number} of {rounds}"
+        text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+        text_x = 10
+        text_y = text_size[1] + 20
+        cv2.putText(frame, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
+
+        if round_number < rounds:
+            text = f"Press 'c' to lock in your move"
+        else:
+            # TODO Make this text announce the actual winner
+            text = f"DONE! Press 'ESC' to exit."
+
+        text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+        text_x = width - text_size[0] - 10
+        text_y = text_size[1] + 10
+        cv2.putText(frame, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
+
+        cv2.imshow("Rock Paper Scissors", frame)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+
+
+play()
